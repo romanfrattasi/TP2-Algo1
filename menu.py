d409528792275e361236f6ceda19fd41c5020c24
Roman Frattasi <romanfrattasi@gmail.com>
2023-05-27 20:35:00 -0300

    Menu de la aplicacion

---
+++ menu.py
@@ -0,0 +1,53 @@
+# Mostrar el usuario que más dinero apostó.
+# 8. Mostrar el usuario que más veces ganó.
+
+
+def menu():
+    OPCIONES = ('Mostrar plantel',
+                'Mostrar tabla de posiciones',
+                'Mostrar informacion de un equipo',
+                'Grafico goles/minutos',
+                'Cargar dinero',
+                'Mostrar el usuario que mas dinero aposto',
+                'Mostrar el usuario que mas veces gano',
+                'Salir'
+                )
+    
+    for i in range(len(OPCIONES)):
+        print(f'{i+1}- {OPCIONES[i]}')
+    opcion = input('Elija una opcion: ')
+    
+    while opcion != '8':
+        if opcion == '1':
+            pass
+        elif opcion =='2':
+            pass
+        elif opcion =='3':
+            pass
+        elif opcion =='4':
+            pass
+        elif opcion =='5':
+            pass
+        elif opcion =='6':
+            pass
+        elif opcion =='7':
+            pass
+        else:
+            print('Opcion incorrecta. Intente otra vez')
+        OPCIONES = ('Mostrar plantel',
+                'Mostrar tabla de posiciones',
+                'Mostrar informacion de un equipo',
+                'Grafico goles/minutos',
+                'Cargar dinero',
+                'Mostrar el usuario que mas dinero aposto',
+                'Mostrar el usuario que mas veces gano',
+                'Salir'
+                )
+        for i in range(len(OPCIONES)):
+            print(f'{i+1}- {OPCIONES[i]}')
+        opcion = input('Elija una opcion: ')
+    
+
+def main():
+    menu()
+main()
